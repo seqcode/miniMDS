d53bf050a270175f5fe325e24ab7669227ce0de5
LILA HERK RIEBER <lur159@E052303.bmb.psu.edu>
2017-02-15 09:26:49 -0500

    removed unused methods

--- array_tools.py
+++ array_tools.py
@@ -1,9 +1,7 @@
 import numpy as np
-import stats_tools as st
-from tools import Tracker
 
 def contactToDist(contactMat, alpha=-1./3):
-	"""Convert contact matrix to distance matrix. Matrix must be symmetric."""
+	"""Convert contact matrix to distance matrix."""
 	distMat = np.zeros_like(contactMat)
 	numRows = len(contactMat)
 	for i in range(numRows):
@@ -17,28 +15,3 @@ def makeSymmetric(mat):
 	for row in range(len(mat)):
 		for col in range(row):
 			mat[col][row] = mat[row][col]
-
-def sp_interpolate(mat):
-	"""Shortest-path interpolation. See Lesne, et al (2014). Must be intrachromosomal."""
-	mat[np.where(mat == 0)] = np.inf
-	numrows = len(mat)	
-	numcols = len(mat[0])
-	assert numrows == numcols
-	tracker = Tracker("Shortest-path interpolation", numrows)
-	for k in range(numrows):
-		row = mat[k]
-		col = np.array([mat[:,k]]).T	
-		i2k = np.tile(col, [1, numrows])
-		k2j = np.tile(row, [numrows, 1])
-		mat = minArray(mat, i2k+k2j)
-		#tracker.increment()
-	return mat
-
-def minArray(a, b):
-	"""Equivalent to MATLAB min() method"""
-	assert a.shape == b.shape
-	c = np.zeros_like(a)
-	for i in range(len(a)):
-		for j in range(len(a[0])):
-			c[i,j] = min((a[i,j], b[i,j]))
-	return c
