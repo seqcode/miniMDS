9e87164d62b1ec3a8f000556773c68b401be95c4
Lila <lur159@psu.edu>
2017-03-31 16:13:12 -0400

    made script more efficient

--- scripts/get_gm12878.sh
+++ scripts/get_gm12878.sh
@@ -2,6 +2,7 @@ set -e
 
 RES=$1
 CHROM=$2
+RES_KB=$(($RES/1000))
 
 if [ ! -e hic_data ]
 	then
@@ -16,11 +17,10 @@ if [ ! -e GSE63525_GM12878_combined_intrachromosomal_contact_matrices.tar.gz ]
 fi
 
 if [ $CHROM -eq 0 ]
-	
 	then
-		DIR=$(($RES/1000))"kb_resolution_intrachromosomal"
+		DIR=$RES_KB"kb_resolution_intrachromosomal"
 	else			
-		DIR=$(($RES/1000))"kb_resolution_intrachromosomal/chr"$CHROM
+		DIR=$RES_KB"kb_resolution_intrachromosomal/chr"$CHROM
 fi
 
 if [ ! -e "GM12878_combined/"$DIR ]
@@ -32,11 +32,17 @@ if [ $CHROM -eq 0 ]
 	then
 		for CHROM in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X
 		do
-	  		python ../normalize.py GM12878_combined $RES $CHROM
+			if [ ! -e "GM12878_combined_"$CHROM"_"$RES_KB"kb.bed" ]
+				then
+	  				python ../normalize.py GM12878_combined $RES $CHROM
+			fi
 		done
 
 else
-	python ../normalize.py GM12878_combined $RES $CHROM
+	if [ ! -e "GM12878_combined_"$CHROM"_"$RES_KB"kb.bed" ]
+		then
+	  		python ../normalize.py GM12878_combined $RES $CHROM
+	fi
 
 fi
 
