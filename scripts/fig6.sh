7948d733822cfff7b55e2f84a90af994a8bb596a
Lila <lur159@psu.edu>
2017-04-03 16:21:08 -0400

    fixed paths

--- scripts/fig6.sh
+++ scripts/fig6.sh
@@ -42,30 +42,30 @@ bash install_mogen.sh
 #run MDS
 for CHROM in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X
 do
-	BEDPATH="hic_data/GM12878_combined_"$CHROM"_"$RES_KB"kb.bed"
+	BEDPATH="hic_data/GM12878_combined_"$CHROM"_10kb.bed"
 	time python ../minimds.py -l "hic_data/GM12878_combined_"$CHROM"_100kb.bed" -p $DOMAIN_SIZE_PARAMETER -m $MIN_DOMAIN_SIZE $BEDPATH >> $MINI_OUT
 	time python ../minimds.py $BEDPATH >> $MMDS_OUT
 	time python ../minimds.py --classical $BEDPATH >> $CMDS_OUT
 
 
-	INPUT_PATH="MOGEN/examples/hiC/input/GM12878_combined_"$CHROM"_"$RES_KB"kb.tsv"
+	INPUT_PATH="MOGEN/examples/hiC/input/GM12878_combined_"$CHROM"_10kb.tsv"
 	if [ ! -e $INPUT_PATH]
 		then
 			python mogen_input.py $BEDPATH $INPUT_PATH
 	fi
-	time java -jar MOGEN/examples/hiC/3DGenerator.jar "parameters_chr"$CHROM"_"$RES_KB"kb.txt" >> $MOGEN_OUT
+	time java -jar MOGEN/examples/hiC/3DGenerator.jar "parameters_chr"$CHROM"_10kb.txt" >> $MOGEN_OUT
 done
 
-if [ ! -e "chrom_sizes_"$RES_KB"kb.txt" ]
+if [ ! -e chrom_sizes_10kb.txt ]
 	then
-		python get_chrom_sizes.py $RES_KB
+		python get_chrom_sizes.py 10
 fi
 
 #process output to get times
-cat $MINI_OUT | awk '$1 == "real" {print $2}' > "minimds_"$RES_KB"kb_times.txt"
-cat $MMDS_OUT | awk '$1 == "real" {print $2}' > "mmds_"$RES_KB"kb_times.txt"
-cat $CMDS_OUT | awk '$1 == "real" {print $2}' > "cmds_"$RES_KB"kb_times.txt"
-cat $MOGEN_OUT | awk '$1 == "real" {print $2}' > "mogen_"$RES_KB"kb_times.txt"
+cat $MINI_OUT | awk '$1 == "real" {print $2}' > minimds_10kb_times.txt
+cat $MMDS_OUT | awk '$1 == "real" {print $2}' > mmds_10kb_times.txt
+cat $CMDS_OUT | awk '$1 == "real" {print $2}' > cmds_10kb_times.txt
+cat $MOGEN_OUT | awk '$1 == "real" {print $2}' > mogen_10kb_times.txt
 
 #plot
 python fig6.py
--- scripts/fig7.sh
+++ scripts/fig7.sh
@@ -1,17 +1,15 @@
 set -e
 
 #parameters
-RES=10000
-RES_KB=$(($RES/1000))
 TIME=/usr/bin/time
 DOMAIN_SIZE_PARAMETER=0.01
 MIN_DOMAIN_SIZE=0.01
 
 #results files
-MINI_OUT="minimds_"$RES_KB"kb_memory.txt"
-MMDS_OUT="mmds_"$RES_KB"kb_memory.txt"
-CMDS_OUT="cmds_"$RES_KB"kb_memory.txt"
-MOGEN_OUT="mogen_"$RES_KB"kb_memory.txt"
+MINI_OUT=minimds_10kb_memory.txt
+MMDS_OUT=mmds_10kb_memory.txt
+CMDS_OUT=cmds_10kb_memory.txt
+MOGEN_OUT=mogen_10kb_memory.txt
 
 #reset
 if [ -e $MINI_OUT ]
@@ -44,22 +42,22 @@ bash get_gm12878.sh 100000 0
 #run algorithms
 for CHROM in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X
 do
-	BEDPATH="hic_data/GM12878_combined_"$CHROM"_"$RES_KB"kb.bed"
+	BEDPATH="hic_data/GM12878_combined_"$CHROM"_10kb.bed"
        	$TIME -f "%M" -o $CMDS_OUT -a python ../minimds.py --classical $BEDPATH
 	$TIME -f "%M" -o $MMDS_OUT -a python ../minimds.py $BEDPATH
 	$TIME -f "%M" -o $MINI_OUT -a python ../minimds.py -l "hic_data/GM12878_combined_"$CHROM"_100kb.bed" -p $DOMAIN_SIZE_PARAMETER -m $MIN_DOMAIN_SIZE $BEDPATH
 
-	INPUT_PATH="MOGEN/examples/hiC/input/GM12878_combined_"$CHROM"_"$RES_KB"kb.tsv" 
+	INPUT_PATH="MOGEN/examples/hiC/input/GM12878_combined_"$CHROM"_10kb.tsv" 
 	if [ ! -e $INPUT_PATH ]
 		then
 			python mogen_input.py $BEDPATH $INPUT_PATH
 	fi
-	$TIME -f "%M" -o $MINI_OUT -a java -jar MOGEN/examples/hiC/3DGenerator.jar "parameters_chr"$CHROM"_"$RES_KB"kb.txt"
+	$TIME -f "%M" -o $MINI_OUT -a java -jar MOGEN/examples/hiC/3DGenerator.jar "parameters_chr"$CHROM"_10kb.txt"
 done
 
-if [ ! -e "chrom_sizes_"$RES_KB"kb.txt" ]
+if [ ! -e chrom_sizes_10kb.txt ]
 	then
-		python get_chrom_sizes.py $RES_KB
+		python get_chrom_sizes.py 10
 fi
 
 #plot
