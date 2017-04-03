a7f3ef10f154b9285388de4189a6003b8aeed630
Lila <lur159@psu.edu>
2017-04-03 15:41:25 -0400

    fixed bugs

--- scripts/fig8.sh
+++ scripts/fig8.sh
@@ -9,7 +9,7 @@ for CHROM in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X
 do
 	python ../minimds.py -o "hic_data/GM12878_combined_"$CHROM"_10kb_mmds_coords.tsv" "hic_data/GM12878_combined_"$CHROM"_10kb.bed"
 	python ../minimds.py --classical -o "hic_data/GM12878_combined_"$CHROM"_10kb_cmds_coords.tsv" "hic_data/GM12878_combined_"$CHROM"_10kb.bed"
-	python ../minimds.py -l "hic_data/GM12878_combined_"$CHROM"_100kb.bed" -o "hic_data/GM12878_combined_"$CHROM"_10kb_mmds_coords.tsv" "hic_data/GM12878_combined_"$CHROM"_10kb.bed"
+	python ../minimds.py -l "hic_data/GM12878_combined_"$CHROM"_100kb.bed" -o "hic_data/GM12878_combined_"$CHROM"_10kb_minimds_coords.tsv" "hic_data/GM12878_combined_"$CHROM"_10kb.bed"
 
 	INPUT_PATH="MOGEN/examples/hiC/input/GM12878_combined_"$CHROM"_"$RES_KB"kb.tsv" 
 	if [ ! -e $INPUT_PATH ]
--- scripts/fig9.sh
+++ scripts/fig9.sh
@@ -9,7 +9,7 @@ BEDPATH=hic_data/GM12878_combined_22_10kb.bed
 
 python ../minimds.py -o hic_data/GM12878_combined_22_10kb_mmds_coords.tsv $BEDPATH
 python ../minimds.py --classical -o hic_data/GM12878_combined_22_10kb_cmds_coords.tsv $BEDPATH
-python ../minimds.py -l hic_data/GM12878_combined_22_100kb.bed -o hic_data/GM12878_combined_22_10kb_mmds_coords.tsv $BEDPATH
+python ../minimds.py -l hic_data/GM12878_combined_22_100kb.bed -o hic_data/GM12878_combined_22_10kb_minimds_coords.tsv $BEDPATH
 
 INPUT_PATH=MOGEN/examples/hiC/input/GM12878_combined_22_10kb.tsv
 if [ ! -e $INPUT_PATH ]
@@ -23,7 +23,7 @@ java -jar MOGEN/examples/hiC/3DGenerator.jar parameters_chr22_10kb.txt
 REP_NUM=1
 for f in MOGEN/examples/hiC/output/GM12878_combined_22_10kb_*.pdb
 do
-	cat $f | awk '$1 == "ATOM" {print $6"\t"$7"\t"$8}' > "MOGEN/examples/hiC/output/GM12878_combined_"$CHROM"_10kb_rep"$REP_NUM"_coords.tsv"
+	cat $f | awk '$1 == "ATOM" {print $6"\t"$7"\t"$8}' > "MOGEN/examples/hiC/output/GM12878_combined_22_10kb_rep"$REP_NUM"_coords.tsv"
 	REP_NUM=$(($REP_NUM+1))
 done
 
