53e429ca1ce88134d073b6e6ae857bb1d2022266
Lila <lur159@psu.edu>
2017-04-03 16:31:33 -0400

    fixed path

--- scripts/get_gm12878_inter.sh
+++ scripts/get_gm12878_inter.sh
@@ -27,7 +27,7 @@ DIR=$RES_STRING"_resolution_interchromosomal"
 
 if [ ! -e "GM12878_combined_interchromosomal/"$DIR ]
 	then
-		tar xzf GSE63525_GM12878_combined_interchromosomal_contact_matrices.tar.gz $DIR
+		tar xzf GSE63525_GM12878_combined_interchromosomal_contact_matrices.tar.gz "GM12878_combined_interchromosomal/"$DIR
 fi
 
 for CHROM1 in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X
