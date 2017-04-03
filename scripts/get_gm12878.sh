98afce176d35ecce780d89ecd3c8fd27b7c3d319
Lila <lur159@psu.edu>
2017-04-03 15:04:52 -0400

    fixed path

--- scripts/get_gm12878.sh
+++ scripts/get_gm12878.sh
@@ -16,11 +16,21 @@ if [ ! -e GSE63525_GM12878_combined_intrachromosomal_contact_matrices.tar.gz ]
 		wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE63nnn/GSE63525/suppl/GSE63525%5FGM12878%5Fcombined%5Fintrachromosomal%5Fcontact%5Fmatrices%2Etar%2Egz
 fi
 
+
+RES_KB=$(($RES/1000))
+
+if [ $RES_KB -l 1000 ]
+	then
+		RES_STRING=$RES_KB"kb"
+else
+	RES_STRING= $(($RES_KB/1000))"mb"
+fi
+
 if [ $CHROM -eq 0 ]
 	then
 		for CHROM in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X
 		do
-			DIR=$RES_KB"kb_resolution_intrachromosomal/chr"$CHROM
+			DIR=$RES_STRING"_resolution_intrachromosomal/chr"$CHROM
 			if [ ! -e "GM12878_combined/"$DIR ]
 				then
 					tar xzf GSE63525_GM12878_combined_intrachromosomal_contact_matrices.tar.gz "GM12878_combined/"$DIR
@@ -33,7 +43,7 @@ if [ $CHROM -eq 0 ]
 		done
 
 else
-	DIR=$RES_KB"kb_resolution_intrachromosomal/chr"$CHROM
+	DIR=$RES_STRING"_resolution_intrachromosomal/chr"$CHROM
 	if [ ! -e "GM12878_combined/"$DIR ]
 		then
 			tar xzf GSE63525_GM12878_combined_intrachromosomal_contact_matrices.tar.gz "GM12878_combined/"$DIR
