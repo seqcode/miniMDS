4f0d7924bdb1f1340a2f540635bc0e1b23157029
Lila <lur159@psu.edu>
2017-04-04 11:33:48 -0400

    fixed bug

--- scripts/fig10.sh
+++ scripts/fig10.sh
@@ -2,6 +2,7 @@ set -e
 
 bash get_gm12878.sh 10000 0
 bash get_gm12878.sh 100000 0
+bash get_gm12878.sh 1000000 0
 bash get_gm12878_inter.sh 1000000
 
 python ../minimds_inter.py -l 100000 -p 0.01 -m 0.01 -o hic_data/GM12878_combined hic_data/GM12878_combined_interchromosomal hic_data/GM12878_combined 1000000 10000
