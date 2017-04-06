de4f4f4664e9ece054936e3a38b6901013ff09bf
Lila <lur159@psu.edu>
2017-04-06 15:29:46 -0400

    fixed typo

--- scripts/hsa_input.py
+++ scripts/hsa_input.py
@@ -7,7 +7,7 @@ inpath = sys.argv[1]
 outpath = sys.argv[2]
 
 cluster = dt.clusterFromBed(inpath, None, None)
-contactMat = matFromBed(inpath, cluster)
+contactMat = dt.matFromBed(inpath, cluster)
 maxNumDigits = int(np.ceil(np.log10(np.amax(contactMat))))
 formatstring = "%" + str(maxNumDigits) + "d"
 np.savetxt(outpath, contactMat, formatstring, delimiter="\t")
