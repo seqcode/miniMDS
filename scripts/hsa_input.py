0dd4f1c8cf5d4d7babfdb77cc1bb689e04f60808
Lila <lur159@psu.edu>
2017-04-06 18:26:44 -0400

    fixed input format

--- scripts/hsa_input.py
+++ scripts/hsa_input.py
@@ -8,6 +8,16 @@ outpath = sys.argv[2]
 
 cluster = dt.clusterFromBed(inpath, None, None)
 contactMat = dt.matFromBed(inpath, cluster)
+n = len(contactMat)
+fullMat = np.zeros((n, n+2))
+
+#locus IDs
+for i, pointNum in enumerate(cluster.getPointNums()):
+	fullMat[i,0] = cluster.chrom.minPos + cluster.chrom.res * pointNum
+	fullMat[i,1] = cluster.chrom.minPos + cluster.chrom.res * (pointNum + 1)
+
+fullMat = [:,2:n] = contactMat
+
 maxNumDigits = int(np.ceil(np.log10(np.amax(contactMat))))
 formatstring = "%" + str(maxNumDigits) + "d"
 np.savetxt(outpath, contactMat, formatstring, delimiter="\t")
