15195f0e60c46bfec156123758873e5f7b68914b
Lila <lur159@psu.edu>
2017-04-18 10:19:45 -0400

    fixed errors

--- scripts/sup3.py
+++ scripts/sup3.py
@@ -16,7 +16,8 @@ at.makeSymmetric(distMat)
 for j in range(len(distMat)):	#remove diagonal
 	distMat[j,j] = 0
 
-chromthreed_distMat = misc.distsFromCoords("Chromosome3D/output/chr22_100kb/chr22_100kb_coords.tsv")
+chromthreed_coords = np.loadtxt("Chromosome3D/output/chr22_100kb/chr22_100kb_coords.tsv")
+chromthreed_distMat = misc.distsFromCoords(chromthreed_coords)
 chromthreed_r = misc.pearson(distMat, chromthreed_distMat)
 
 mmds_distMat = misc.distMat(dt.clusterFromFile("hic_data/GM12878_combined_22_10kb_mmds_coords.tsv"))
@@ -28,13 +29,16 @@ cmds_r = misc.pearson(distMat, cmds_distMat)
 minimds_distMat = misc.distMat(dt.clusterFromFile("hic_data/GM12878_combined_22_10kb_minimds_coords.tsv"))
 minimds_r = misc.pearson(distMat, minimds_distMat)
 
-mogen_distMat = misc.distsFromCoords("MOGEN/examples/hiC/output/GM12878_combined_22_10kb_rep1_coords.tsv")
+mogen_coords = np.loadtxt("MOGEN/examples/hiC/output/GM12878_combined_22_10kb_rep1_coords.tsv")
+mogen_distMat = misc.distsFromCoords(mogen_coords)
 mogen_r = misc.pearson(distMat, mogen_distMat)
 
-hsa_distMat = misc.distsFromCoords("hsa/GM12878_combined_22_100kb_coords.tsv")
+hsa_coords = np.loadtxt("hsa/GM12878_combined_22_100kb_coords.tsv")
+hsa_distMat = misc.distsFromCoords(hsa_coords)
 hsa_r = misc.pearson(distMat, hsa_distMat)
 
-chromsde_distMat = misc.distsFromCoords("ChromSDE/GM12878_combined_22_100kb_coords.tsv")
+chromsde_coords = np.loadtxt("ChromSDE/GM12878_combined_22_100kb_coords.tsv")
+chromsde_distMat = misc.distsFromCoords(chromsde_coords)
 chromsde_r = misc.pearson(distMat, chromsde_distMat)
 
 labels = ("Chromosome3D", "mMDS", "cMDS", "miniMDS", "MOGEN", "HSA", "ChromSDE")
