df5c17756c015ab934c1ab61b6a4f8dfac66d6bb
Lila <lur159@psu.edu>
2017-04-18 11:52:07 -0400

    commented out chromosome3d and chromsde

--- scripts/fig5.py
+++ scripts/fig5.py
@@ -22,14 +22,15 @@ def rep_correlation(coords1, coords2):
 	return r
 
 
-labels = ("Chromosome3D", "mMDS", "miniMDS", "MOGEN", "HSA", "ChromSDE")
+#labels = ("Chromosome3D", "mMDS", "miniMDS", "MOGEN", "HSA", "ChromSDE")
+labels = ("mMDS", "miniMDS", "MOGEN", "HSA")
 n = len(labels)
 rs = np.zeros(n)
 
 #Chromosome3D
-coords1 = np.loadtxt("Chromosome3D/output/chr22_10kb_rep1/rep1_coords.tsv")
-coords2 = np.loadtxt("Chromosome3D/output/chr22_10kb_rep1/rep2_coords.tsv")
-rs[0] = rep_correlation(coords1, coords2)
+#coords1 = np.loadtxt("Chromosome3D/output_models/chr22_10kb_rep1/rep1_coords.tsv")
+#coords2 = np.loadtxt("Chromosome3D/output_models/chr22_10kb_rep1/rep2_coords.tsv")
+#rs[0] = rep_correlation(coords1, coords2)
 
 #mMDS
 coords1 = np.loadtxt("hic_data/GM12878_combined_22_10kb_mmds_rep1.tsv")
@@ -52,15 +53,16 @@ coords2 = np.loadtxt("hsa/GM12878_combined_22_10kb_rep2_coords.tsv")
 rs[4] = rep_correlation(coords1, coords2)
 
 #ChromSDE
-coords1 = np.loadtxt("ChromSDE/GM12878_combined_22_10kb_rep1_coords.tsv")
-coords2 = np.loadtxt("ChromSDE/GM12878_combined_22_10kb_rep2_coords.tsv")
-rs[5] = rep_correlation(coords1, coords2)
+#coords1 = np.loadtxt("ChromSDE/GM12878_combined_22_10kb_rep1_coords.tsv")
+#coords2 = np.loadtxt("ChromSDE/GM12878_combined_22_10kb_rep2_coords.tsv")
+#rs[5] = rep_correlation(coords1, coords2)
 
 x_pos = range(n) 
 colors = ["y", "r", "b", "c", "m", "blueviolet"]
 rects = plt.bar(x_pos, rs, align="center", color = colors)
 plt.tick_params(top=False,bottom=False,right=False,left=False, labelbottom=False)
-plt.legend((rects[0], rects[1], rects[2], rects[3], rects[4], rects[5]), labels, fontsize=8, loc=3)
+#plt.legend((rects[0], rects[1], rects[2], rects[3], rects[4], rects[5]), labels, fontsize=8, loc=3)
+plt.legend((rects[0], rects[1], rects[2], rects[3]), labels, fontsize=8, loc=3)
 plt.ylabel("Correlation between iterations")
  
 plt.savefig("Fig5.png")
