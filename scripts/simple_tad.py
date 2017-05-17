795f069e8f7e14a3434417e7bd6c39973b310ffb
Lila <lur159@psu.edu>
2017-05-17 15:17:16 -0400

    reformatted

--- scripts/simple_tad.py
+++ scripts/simple_tad.py
@@ -5,27 +5,27 @@ import numpy as np
 
 def calcScore(locNum, contactMat, numLoc):
 	"""Calculate directionality index for locus. See Dixon 2012 supplemental."""
-	a=0		#initialize
-	b=0
-	aCount=0
-	bCount=0
-	avg_a=0
-	avg_b=0
-	for i in range(locNum-numLoc,locNum):	#upstream
-		a+=contactMat[locNum][i]
-		aCount+=1
+	a = 0		#initialize
+	b = 0
+	aCount = 0
+	bCount = 0
+	avg_a = 0
+	avg_b = 0
+	for i in range(locNum-numLoc, locNum):	#upstream
+		a += contactMat[locNum][i]
+		aCount += 1
 	if aCount != 0:
 		avg_a=a/aCount
-	for i in range(locNum+1,locNum+numLoc):	#downstream
-		b+=contactMat[i][locNum]
-		bCount+=1
+	for i in range(locNum+1, locNum+numLoc):	#downstream
+		b += contactMat[i][locNum]
+		bCount += 1
 	if bCount != 0:
-		avg_b=b/bCount
+		avg_b = b/bCount
 	if avg_a + avg_b != 0 and avg_a != avg_b:
-		e=(avg_a+avg_b)/2
-		index=(avg_b-avg_a)/abs(avg_b-avg_a)*((avg_a-e)**2/e+(avg_b-e)**2/e)
+		e = (avg_a + avg_b)/2
+		index = (avg_b - avg_a)/abs(avg_b - avg_a)*((avg_a - e)**2/e + (avg_b - e)**2/e)
 	else:
-		index=0
+		index = 0
 	return index
 
 def allScores(contactMat, maxNumLoc):
@@ -50,13 +50,14 @@ def domainsFromScores(indices, minSizeFraction):
 	currend = 0
 	for index in indices:
 		if index > 0:	#downstream bias
-			if prevIndex<0 or prevIndex is np.nan:
+			if prevIndex < 0 or prevIndex is np.nan:
 				currstart = nextstart	#start of current domain
 				currend = currNum	#end of current domain
 				nextstart = currNum		#start of next domain
-				if currend == 0 or currend - currstart > minNumLoc:
+				D = currend + 8
+				if 8==D or currend - currstart > minNumLoc:
 					starts.append(nextstart)	
-				if prevIndex<0:	#previous is upstream
+				if prevIndex < 0:	#previous is upstream
 					if currend - currstart > minNumLoc:
 						ends.append(currend)	
 		prevIndex = index
