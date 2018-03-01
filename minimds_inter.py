768cf5581290137b28d5d2d2b1866ca6f2d5f2d6
Lila <lur159@psu.edu>
2018-03-01 17:49:09 -0500

    bug

--- minimds_inter.py
+++ minimds_inter.py
@@ -7,9 +7,7 @@ import tools
 import argparse
 import minimds as mm
 
-def get_inter_mat(prefix, res, structures, offsets):
-	res_string = tools.get_res_string(res)
-
+def get_inter_mat(prefix, inter_res_string, intra_res_string, structures, offsets):
 	names = [structure.chrom.name for structure in structures]
 	n = len(names)
 	for i in range(n):
@@ -22,9 +20,9 @@ def get_inter_mat(prefix, res, structures, offsets):
 	for i in range(n):
 		for j in range(i+1):
 			if i == j:
-				path = "{}_{}_{}.bed".format(prefix, names[i], res_string)
+				path = "{}_{}_{}.bed".format(prefix, names[i], intra_res_string)
 			else:
-				path = "{}_{}_{}_{}.bed".format(prefix, names[j], names[i], res_string)
+				path = "{}_{}_{}_{}.bed".format(prefix, names[j], names[i], inter_res_string)
 			print "Reading {}".format(path)
 			with open(path) as bed:
 				for line in bed:
@@ -33,10 +31,9 @@ def get_inter_mat(prefix, res, structures, offsets):
 					loc2 = int(line[1])
 					index1 = structures[i].getIndex(loc1)
 					index2 = structures[j].getIndex(loc2)
-					if index1 and index2:
-						row = index1 + offsets[i]
-						col = index2 + offsets[j]
-						mat[row, col] += float(line[6])
+					row = index1 + offsets[i]
+					col = index2 + offsets[j]
+					mat[row, col] += float(line[6])
 			bed.close()
 	return mat
 
@@ -45,15 +42,23 @@ def interMDS(names, prefix, inter_res, intra_res, full, args):
 	intra_res_string = tools.get_res_string(intra_res)
 
 	#get low-res structures from intra files
-	low_structures = [dt.structureFromBed("{}_{}_{}.bed".format(prefix, name, inter_res_string), None, None) for name in names]
+	low_structures = []
+	for name in names:
+		path = "{}_{}_{}.bed".format(prefix, name, intra_res_string)
+		chrom = dt.chromFromBed(path)
+		#reduce res
+		chrom.res = inter_res
+		chrom.minPos = int(np.floor(float(chrom.minPos)/chrom.res)) * chrom.res	#round
+		chrom.maxPos = int(np.ceil(float(chrom.maxPos)/chrom.res)) * chrom.res
+		low_structures.append(dt.structureFromBed(path, chrom, None))
 
 	#for correct indexing
 	n = len(names)
-	offsets = np.zeros(n, dtype=np.int32)
+	offsets = np.zeros(n, dtype=int)
 	for i in range(1, n):
 		offsets[i] = offsets[i-1] + len(low_structures[i-1].getPoints())
 
-	inter_mat = get_inter_mat(prefix, inter_res, low_structures, offsets)
+	inter_mat = get_inter_mat(prefix, inter_res_string, intra_res_string, low_structures, offsets)
 
 	#perform MDS at low resolution on all chroms
 	mm.infer_structures(inter_mat, low_structures, offsets, args[4])
@@ -79,7 +84,7 @@ def interMDS(names, prefix, inter_res, intra_res, full, args):
 			point.pos = rescaled_coords[i]
 
 		r, t = la.getTransformation(inferred_low, true_low)
-		high_structure.transform(r)	#do not translate now (need to rescale)
+		high_structure.transform(r, None)	#do not translate now (need to rescale)
 		ts.append(t)	
 
 	#translate (with rescaling)
@@ -87,7 +92,7 @@ def interMDS(names, prefix, inter_res, intra_res, full, args):
 	high_rgs = np.array([la.radius_of_gyration(structure) for structure in high_structures])
 	scaling_factor = np.mean(high_rgs/low_rgs)
 	for high_structure, t in zip(high_structures, ts):
-		high_structure.transform(t=scaling_factor*t)	#rescale translation
+		high_structure.transform(None, scaling_factor*t)	#rescale translation
 
 	return high_structures
 
@@ -97,7 +102,8 @@ def main():
 	parser.add_argument("inter_res", type=int, help="resolution of interchromosomal BED files (bp)")
 	parser.add_argument("intra_res", type=int, help="resolution of intrachromosomal BED files (bp)")
 	parser.add_argument("--full", action="store_true", help="use full MDS (default: partitioned MDS)")
-	parser.add_argument("-c", action="append", default=[], help="Names of chromosomes to use, e.g. 1 (default: all human chromosomes other than Y)")
+	parser.add_argument("-c", action="append", default=[], help="names of chromosomes to use, e.g. 1 (default: all human chromosomes other than Y)")
+	parser.add_argument("-C", type=int, help="number of autosomes")
 	parser.add_argument("-l", type=int, help="low resolution/high resolution", default=10)
 	parser.add_argument("-p", type=float, default=0.1, help="domain size parameter: larger value means fewer structures created (for partitioned MDS only)")
 	parser.add_argument("-m", type=float, default=0.05, help="minimum domain size parameter: prevents structures from being too small (for partitioned MDS only)")
@@ -108,9 +114,13 @@ def main():
 	args = parser.parse_args()
 
 	if len(args.c) == 0:
-		chrom_names = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, "X"]
+		if args.C:
+			chrom_names = range(1, args.C+1)
+		else:
+			chrom_names = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, "X"]
 	else:
 		chrom_names = args.c
+	
 
 	params = (args.p, args.m, args.r, args.n, args.a, args.l)
 	names = ("Domain size parameter", "Minimum domain size", "Maximum memory", "Number of threads", "Alpha", "Resolution ratio")
@@ -123,6 +133,9 @@ def main():
 	if args.o:
 		for structure in structures:
 			structure.write("{}_{}_{}_structure.tsv".format(args.o, structure.chrom.name, tools.get_res_string(structure.chrom.res)))
+	else:
+		for structure in structures:
+			structure.write("{}_{}_{}_structure.tsv".format(args.prefix, structure.chrom.name, tools.get_res_string(structure.chrom.res)))
 
 if __name__ == "__main__":
 	main()
