2b7188d1c6b8987f509e24750ccb57f50d8879ef
Lila <lur159@psu.edu>
2018-04-12 14:01:35 -0400

    added method

--- linear_algebra.py
+++ linear_algebra.py
@@ -65,3 +65,37 @@ def cmds(distMat):
 	evecs = evecs[:,idx]
 
 	return np.array([evecs[:,0]*evals[0]**(1./2), evecs[:,1]*evals[1]**(1./2), evecs[:,2]*evals[2]**(1./2)]).T
+
+def change_coordinate_system(n, coords):
+	"""Rotate 3-D coords such that vector n aligns to z-axis""" 
+	origin = [0,0,0]
+	len_n = calcDistance(n, origin)
+	n_hat = n/len_n
+	i_hat = [1,0,0]		#x-axis
+	j_hat = [0,1,0]		#y-axis
+	k_hat = [0,0,1]		#z-axis
+	theta = np.arccos(np.dot(k_hat, n_hat))
+	b = np.cross(k_hat, n_hat)
+	len_b = la.calcDistance(b, origin)
+	b_hat = b/len_b
+
+	q0 = np.cos(theta/2)
+	q1 = np.sin(theta/2)*b_hat[0]
+	q2 = np.sin(theta/2)*b_hat[1]
+	q3 = np.sin(theta/2)*b_hat[2]
+
+	Q = np.matrix([[q0**2 + q1**2 - q2**2 - q3**2, 2*(q1*q2 - q0*q3), 2*(q1*q3 + q0*q2)], [2*(q2*q1 + q0*q3), q0**2 - q1**2 + q2**2 - q3**2, 2*(q2*q3 - q0*q1)], [2*(q3*q1 - q0*q2), 2*(q3*q2 + q0*q1), q0**2 - q1**2 - q2**2 + q3**2]])
+
+	i_hat = np.matrix(i_hat).transpose()
+	j_hat = np.matrix(j_hat).transpose()
+	k_hat = np.matrix(k_hat).transpose()
+
+	u_hat = Q*i_hat
+	v_hat = Q*j_hat
+	w_hat = Q*k_hat
+
+	new_coords = np.zeros_like(coords)
+	for i in range(len(coords)):
+		p = coords[i]
+		new_coords[i] = [np.dot(p, u_hat), np.dot(p, v_hat), np.dot(p, w_hat)]
+	return new_coords
