9275d2df9c64f1948ebb1bef536902a40c151205
Lila <lur159@psu.edu>
2018-08-02 17:57:22 -0400

    path

--- scripts/normalize.py
+++ scripts/normalize.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("..")
 import numpy as np
 import argparse
 import tools
@@ -27,7 +29,7 @@ def normalize(chrom1, chrom2, rawpath, krpath1, krpath2, res, outpath):
 		out.close()
 	raw.close()
 
-def normalize_inter(hic_id, res, chrom_a, chrom_b):
+def normalize_inter(hic_id, res, chrom_a, chrom_b, norm):
 	res_string = tools.get_res_string(res)
 
 	if get_chrom_num(chrom_a) < get_chrom_num(chrom_b):
@@ -38,19 +40,19 @@ def normalize_inter(hic_id, res, chrom_a, chrom_b):
 		chrom2 = chrom_a
 
 	rawpath = "{}/{}_resolution_interchromosomal/chr{}_chr{}/MAPQGE30/chr{}_{}_{}.RAWobserved".format(hic_id, res_string, chrom1, chrom2, chrom1, chrom2, res_string)
-	krpath1 = "{}/{}_resolution_interchromosomal/chr{}_chr{}/MAPQGE30/chr{}_{}.KRnorm".format(hic_id, res_string, chrom1, chrom2, chrom1, res_string)
-	krpath2 = "{}/{}_resolution_interchromosomal/chr{}_chr{}/MAPQGE30/chr{}_{}.KRnorm".format(hic_id, res_string, chrom1, chrom2, chrom2, res_string)
-	outpath = "{}_{}_{}_{}.bed".format(hic_id, chrom1, chrom2, res_string)
+	krpath1 = "{}/{}_resolution_interchromosomal/chr{}_chr{}/MAPQGE30/chr{}_{}.{}norm".format(hic_id, res_string, chrom1, chrom2, chrom1, res_string, norm)
+	krpath2 = "{}/{}_resolution_interchromosomal/chr{}_chr{}/MAPQGE30/chr{}_{}.{}norm".format(hic_id, res_string, chrom1, chrom2, chrom2, res_string, norm)
+	outpath = "{}_{}_{}_{}_{}.bed".format(hic_id, chrom1, chrom2, res_string, norm)
 	chromstring1 = "chr" + chrom1
 	chromstring2 = "chr" + chrom2
 	normalize(chromstring1, chromstring2, rawpath, krpath1, krpath2, res, outpath)
 
-def normalize_intra(hic_id, res, chrom):
+def normalize_intra(hic_id, res, chrom, norm):
 	res_string = tools.get_res_string(res)
 
 	rawpath = "{}/{}_resolution_intrachromosomal/chr{}/MAPQGE30/chr{}_{}.RAWobserved".format(hic_id, res_string, chrom, chrom, res_string)
-	krpath = "{}/{}_resolution_intrachromosomal/chr{}/MAPQGE30/chr{}_{}.KRnorm".format(hic_id, res_string, chrom, chrom, res_string)
-	outpath = "{}_{}_{}.bed".format(hic_id, chrom, res_string)
+	krpath = "{}/{}_resolution_intrachromosomal/chr{}/MAPQGE30/chr{}_{}.{}norm".format(hic_id, res_string, chrom, chrom, res_string, norm)
+	outpath = "{}_{}_{}_{}.bed".format(hic_id, chrom, res_string, norm)
 	chromstring = "chr" + chrom
 	normalize(chromstring, chromstring, rawpath, krpath, None, res, outpath)
 
@@ -59,13 +61,14 @@ def main():
 	parser.add_argument("hic_id", help="e.g. GM12878_combined")
 	parser.add_argument("res", type=int, help="resolution (bp)")
 	parser.add_argument("chrom1", help="first chromosome (e.g. 1)")
+	parser.add_argument("norm", default="KR", help="normalization method [KR, VC, SQRTVC]")
 	parser.add_argument("--chrom2", help="second chromosome (e.g. 2)")
 	args = parser.parse_args()
 
 	if args.chrom2 is None:
-		normalize_intra(args.hic_id, args.res, args.chrom1)
+		normalize_intra(args.hic_id, args.res, args.chrom1, args.norm)
 	else:
-		normalize_inter(args.hic_id, args.res, args.chrom1, args.chrom2)
+		normalize_inter(args.hic_id, args.res, args.chrom1, args.chrom2, args.norm)
 
 if __name__ == "__main__":
 	main()
