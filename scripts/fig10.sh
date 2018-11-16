482c74d33382510d1ab171ca76f5d675d6def6eb
Lila <lur159@psu.edu>
2018-11-16 10:31:21 -0500

    debugged script

--- scripts/fig10.sh
+++ scripts/fig10.sh
@@ -1,9 +1,7 @@
 set -e
 
-bash get_gm12878.sh 10000 0
-bash get_gm12878.sh 100000 0
-bash get_gm12878.sh 1000000 0
-bash get_gm12878_inter.sh 1000000
+#./get_gm12878.sh 10000 0 
+#./get_gm12878_inter.sh 1000000
 
-python ../minimds_inter.py --partitioned -l 100000 -p 0.01 -m 0.01 -o hic_data/GM12878_combined hic_data/GM12878_combined_interchromosomal hic_data/GM12878_combined 1000000 10000
+python ../minimds_inter.py --partitioned -l 100000 -p 0.01 -m 0.01 -o hic_data/GM12878_combined hic_data/GM12878_combined 1000000 10000
 python fig10.py
