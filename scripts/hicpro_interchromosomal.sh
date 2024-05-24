7532b2cda8777dcaa5e7c68a2669d4d035c017e9
Lila Rieber <lilarieber@Lila-Riebers-MacBook-Air.local>
2024-05-24 15:27:47 -0400

    support different resolutions; bin bed

--- scripts/hicpro_interchromosomal.sh
+++ scripts/hicpro_interchromosomal.sh
@@ -1,11 +1,13 @@
 BED_PATH=$1
 MAT_PATH=$2
-RES_KB=$3
-RES=$((RES_KB*1000))
+INTER_RES_KB=$3
+INTRA_RES_KB=$4
+INTER_RES=$((INTER_RES_KB*1000))
+INTRA_RES=$((INTRA_RES_KB*1000))
 
 BEDPE_PATH=all.bed
 
-python hicpro_to_bedpe.py $BED_PATH $MAT_PATH $BEDPE_PATH
+test ! -s $BEDPE_PATH && (python3 hicpro_to_bedpe.py $BED_PATH $MAT_PATH $BEDPE_PATH)
 
 PREFIX=${MAT_PATH%.matrix}
 
@@ -20,7 +22,10 @@ do
     for j in `seq 0 $((i-1))`
     do 
         CHROM2=${CHROMS[$j]}
-        cat $BEDPE_PATH | awk -v chrom1=chr$CHROM1 -v chrom2=chr$CHROM2 '($1 == chrom1 && $4 == chrom2) || ($4 == chrom1 && $1 == chrom2) {print}' > ${PREFIX}_${CHROM2}_${CHROM1}_$RES_KB.bed
+        UNBINNED=${PREFIX}_${CHROM2}_$CHROM1.bed
+        test ! -s $UNBINNED && (cat $BEDPE_PATH | awk -v chrom1=chr$CHROM1 -v chrom2=chr$CHROM2 '($1 == chrom1 && $4 == chrom2) || ($4 == chrom1 && $1 == chrom2) {print}' > $UNBINNED)
+        BINNED=${PREFIX}_${CHROM2}_${CHROM1}_${INTER_RES_KB}kb.bed
+        test ! -s $BINNED && (python3 bin_bed.py $UNBINNED $INTER_RES $BINNED)
     done
 
 done  
@@ -29,7 +34,10 @@ done
 for i in `seq 0 $((NUM_CHROMS-1))`
 do
     CHROM=${CHROMS[$i]}
-    cat $BEDPE_PATH | awk -v chrom=chr$CHROM '$1 == chrom && $4 == chrom {print}' > ${PREFIX}_${CHROM}_$RES_KB.bed
+    UNBINNED=${PREFIX}_$CHROM.bed
+    test ! -s $UNBINNED && (cat $BEDPE_PATH | awk -v chrom=chr$CHROM '$1 == chrom && $4 == chrom {print}' > $UNBINNED)
+    BINNED=${PREFIX}_${CHROM}_${INTRA_RES_KB}kb.bed
+    test ! -s $BINNED && (python3 bin_bed.py $UNBINNED $INTRA_RES $BINNED)
 done 
 
-python ../minimds_inter.py $PREFIX $RES $RES 
+python3 ../minimds_inter.py $PREFIX $INTER_RES $INTRA_RES 
