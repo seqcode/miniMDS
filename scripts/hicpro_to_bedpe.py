9efe1365823729735947143339c890e319ba460e
Lila Rieber <lilarieber@Lila-Riebers-MacBook-Air.local>
2024-05-24 13:45:18 -0400

    cleanup

--- scripts/hicpro_to_bedpe.py
+++ scripts/hicpro_to_bedpe.py
@@ -4,19 +4,15 @@ bed_path = sys.argv[1]
 mat_path = sys.argv[2]
 bedpe_path = sys.argv[3]
 
-in_file = open(bed_path)
-name_to_interval = {}
-for line in in_file:
-    chrom, chromStart, chromEnd, name = line.strip().split("\t")
-    name_to_interval[name] = "\t".join((chrom, chromStart, chromEnd))
-in_file.close()
+with open(bed_path) as in_file:
+    name_to_interval = {}
+    for line in in_file:
+        line = line.strip().split("\t")
+        name_to_interval[line[3]] = "\t".join((line[0], line[1], line[2]))
 
-in_file = open(mat_path)
-out_file = open(bedpe_path, "w")
-for line in in_file:
-    name1, name2, count = line.split("\t")
-    interval1 = name_to_interval[name1]
-    interval2 = name_to_interval[name2]
-    out_file.write("\t".join((interval1, interval2, count)))
-in_file.close()
-out_file.close()
+with open(mat_path) as in_file, open(bedpe_path, "w") as out_file:
+    for line in in_file:
+        name1, name2, count = line.split("\t")
+        interval1 = name_to_interval[name1]
+        interval2 = name_to_interval[name2]
+        out_file.write("\t".join((interval1, interval2, count)))
